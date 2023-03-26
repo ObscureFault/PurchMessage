e2b84aaece165b276caeb0373e6a03bb5cd83f88
ObscureFault <obscurefault@gmail.com>
2023-03-26 17:20:34 -0600

    display

--- www/html/display/index.php
+++ www/html/display/index.php
@@ -13,14 +13,52 @@
 
 */
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>JSON Data and Columns Example</title>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script>
+    function createMSG(item)
+    {
+      var div = $('<div>').css({
+        'width': '400px',
+        'height': '100px',
+        'background-color': '#f9f9f9',
+        'border': '1px solid #ddd',
+        'border-radius': '5px',
+        'padding': '10px'
+      });
 
+      var img = $('<img>').attr({
+        'src': '/assets/ltt.jpg',
+        'width': '50px',
+        'height': '50px'
+      }).css({
+        'float': 'left',
+        'margin-right': '10px'
+      });
 
+      var from = $('<h3>').text('New from:').css({
+        'font-size': '20px',
+        'font-weight': 'bold',
+        'margin': '0 0 5px 0'
+      });
 
+      var message = $('<p>').text(').css({
+        'font-size': '16px',
+        'margin': '0'
+      });
 
+      div.append(img);
+      div.append(from);
+      div.append(message);
 
-
-
-
-
-
-
+      return div;
+  });
+</script>
+</head>
+<body>
+</body>
+</html>
